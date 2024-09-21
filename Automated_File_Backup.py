53880bf8d785d761f197509cfa42f4b15027bae8
Jeswanth <63939480+Jeswanth-009@users.noreply.github.com>
2024-09-21 18:40:09 +0530

    Update Automated_File_Backup.py

--- Automated_File_Backup.py
+++ Automated_File_Backup.py
@@ -17,4 +17,13 @@ def copy_folder_to_destination(source, dest):
     except FileExistsError:
         print(f"Folder exists in: {dest}")
         
+def l():
+    copy_folder_to_destination(source_dir, destination_dir)
+        
+schedule.every().day.at("18:40:00").do(l)
+        
 copy_folder_to_destination(source_dir, destination_dir)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
